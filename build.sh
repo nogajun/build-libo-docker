d221e5f32a21ea701da1cbfa2c702ad73c4bb792
Jun NOGATA <nogajun@gmail.com>
2019-10-08 11:46:04 +0900

    Add container stop/rm and log output

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-docker run -it \
+NAME="libobuild"
+ID=$(docker ps -aq -f "name=${NAME}")
+
+[ "${ID}" ] && docker stop ${ID} && docker rm ${ID}
+
+docker run \
+  --name ${NAME} \
   --cap-add SYS_ADMIN \
   --security-opt apparmor:unconfined \
   --user $(id -u):$(id -g) \
   -v $PWD/libreoffice:/libreoffice \
   libo-build \
-  /bin/bash -c 'cd /libreoffice && ./autogen.sh && make'
+  /bin/bash -c 'cd /libreoffice && ./autogen.sh && make' 2>&1 | tee log.$(date "+%Y%m%d-%H%M%S")
